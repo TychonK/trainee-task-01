533b4fa0c47d1b6fd7015b7ace070ec669964634
Tychon <tisha.kruglyak@gmail.com>
2022-04-02 18:09:45 +0200

    category and archive added
    category selection and edit added. also archive added

--- index.js
+++ index.js
@@ -3,20 +3,35 @@ const addBtn = document.getElementById('addBtn');
 const storage = [
     {
         text: 'Lorem ipsum',
-        time: '2022-2-3 | 16:21'
+        time: '2022-2-3 | 16:21',
+        category: 'task',
+        archived: false
     },
     {
         text: 'Lorem ipsum',
-        time: '2022-2-3 | 12:33'
+        time: '2022-2-3 | 12:33',
+        category: 'idea',
+        archived: false
     },
     {
         text: 'Lorem ipsum',
-        time: '2022-2-2 | 11:33'
+        time: '2022-2-2 | 11:33',
+        category: 'random thought',
+        archived: false
     },
     {
         text: 'Lorem ipsum',
         time: '2022-2-1 | 13:53',
-        dates: '2022-02-05 | 12:00'
+        category: 'idea',
+        dates: '2022-02-05 | 12:00',
+        archived: false
+    },
+    {
+        text: 'Lorem ipsum',
+        time: '2022-2-10 | 10:00',
+        category: 'task',
+        dates: '2022-12-20 | 14:15',
+        archived: true
     }
 ]
 
@@ -28,7 +43,8 @@ addBtn.addEventListener('click', function(){
 	let notesObj;
     const addNote = document.getElementById('addNote');
     const addDate = document.getElementById('input-date');
-    const addTime = document.getElementById('input-time')
+    const addTime = document.getElementById('input-time');
+    const category = document.getElementById('selection')
 	let notesString = storage;
 	
 	if(notesString == null){
@@ -42,6 +58,7 @@ addBtn.addEventListener('click', function(){
 	let now = new Date();
 	let dateTime = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} | ${now.getHours()}:${now.getMinutes()}`;
 	
+    //manage dates
     let appointment;
     if (addDate.value.length === 0 && addTime.value.length === 0) {
         appointment = null
@@ -51,8 +68,15 @@ addBtn.addEventListener('click', function(){
     } else {
         appointment = `${addDate.value} | ${addTime.value}`
     }
+
+    //handle categories
+    if (category.value == 'null') {
+        alert('Please select the category')
+        return
+    }
+
 	//pushing into storage
-	let tempObj = { text: addNote.value, time: dateTime, dates: appointment };
+	let tempObj = { text: addNote.value, time: dateTime, category: category.value, dates: appointment };
 	
     storage.push(tempObj);
 	
@@ -76,18 +100,24 @@ function displayNotes(){
 	
 	let html = '';
 	
-	notesObj.forEach(function(note, index){
-		html += `
+    notesObj.forEach(function (note, index) {
+        
+        if (!note.archived) {
+            html += `
 				<div class="card mx-4 my-2 bg-dark text-white thatsMyNote" style="width: 18rem;">
 					<div class="card-body">
 						<h6>${note.time}</h6>
 						<p class="card-text">${note.text.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</p>
-                        ${note.dates ? `<p class="card-subtitle mb-2 text-muted">DATE: ${note.dates}</p>` : ''}
+                        ${note.dates ? `<p class="card-subtitle mb-2 text-warning">DATE: ${note.dates}</p>` : ''}
+                        <p class="card-text text-info">Category: ${note.category}</p>
                         <button id="${index}" onclick=deleteNote(this.id) class="btn btn-danger">Delete</button>
                         <button id="${index}" onclick=openModal(this.id) class="btn btn-info">Edit</button>
 					</div>
 				</div>
 			`;
+        } else {
+            return
+        }
 	});
 	
 	let noteEle = document.getElementById('notes');
@@ -115,13 +145,14 @@ function openModal(id) {
     const date = document.getElementById('modal-input-date')
     const time = document.getElementById('modal-input-time')
     const text = document.getElementById('modal-input-text')
+    const category = document.getElementById('modal-selection')
 
     const saveBtn = `<button id="${id}" onclick="saveChangedNote(this.id)" type="button" class="btn btn-primary" id="saveChangedBtn">Save changes</button>`
     document.querySelector('.modal-footer').insertAdjacentHTML('beforeend', saveBtn);
 
-
     modal.style.display = 'block' 
     text.value = storage[id].text
+    category.value = storage[id].category
     if (!storage[id].dates) {
         return
     }
@@ -134,9 +165,25 @@ function saveChangedNote(id) {
     const date = document.getElementById('modal-input-date')
     const time = document.getElementById('modal-input-time')
     const text = document.getElementById('modal-input-text')
+    const category = document.getElementById('modal-selection')
 
+    if (category.value == 'null') {
+        alert('Select a proper category')
+        return
+    }
+    storage[id].category = category.value  
     storage[id].text = text.value
-    storage[id].dates = `${date.value} | ${time.value}`
+
+    let appointment
+    if (date.value.length === 0 && time.value.length === 0) {
+        appointment = null
+    } else if (date.value.length === 0 || time.value.length === 0) {
+        alert('Please enter both - date and time')
+        return
+    } else {
+        appointment = `${date.value} | ${time.value}`
+    }
+    storage[id].dates = appointment
 
     closeModal()
     displayNotes()
